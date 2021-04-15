73738e1f43e821321f88b1944d8a48cb78efb4be
navayuvansb <sb.navayuvan@gmail.com>
2021-04-15 11:50:23 +0530

    Define filters for Bookmark Detail View

---
+++ bookmarks/filters.py
@@ -0,0 +1,23 @@
+import django_filters
+from django import forms
+from .models import Bookmark
+
+
+class BookmarkFilter(django_filters.FilterSet):
+
+    CHOICES = (
+        ("ascending", "A-Z"),
+        ("descending", "Z-A"),
+    )
+
+    sort_by = django_filters.ChoiceFilter(
+        label="Sort By", method="sort_by_preference", choices=CHOICES
+    )
+
+    class Meta:
+        model = Bookmark
+        fields = {"name": ["icontains"]}
+
+    def sort_by_preference(self, queryset, name, value):
+        expression = "name" if value == "ascending" else "-name"
+        return queryset.order_by(expression)
