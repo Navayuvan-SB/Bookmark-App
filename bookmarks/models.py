c7c563baa5fc6f635cf5f0315a766521ec013451
navayuvansb <sb.navayuvan@gmail.com>
2021-04-15 10:44:48 +0530

    Define Bookmark model

--- bookmarks/models.py
+++ bookmarks/models.py
@@ -1,3 +1,16 @@
 from django.db import models
+from taggit.managers import TaggableManager
 
-# Create your models here.
+
+class Bookmark(models.Model):
+
+    name = models.CharField(max_length=100)
+    description = models.TextField()
+    url = models.URLField()
+
+    tags = TaggableManager()
+
+    def __str__(self):
+        return self.name
+
+    
