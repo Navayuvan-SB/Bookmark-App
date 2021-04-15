f0a2de5b0e1b3b97ac5adec7f461bff78918de8b
navayuvansb <sb.navayuvan@gmail.com>
2021-04-15 11:51:56 +0530

    Define view to Bookmark List View

--- bookmarks/views.py
+++ bookmarks/views.py
@@ -1,3 +1,16 @@
 from django.shortcuts import render
+from django.views import generic
+from .models import Bookmark
+from .filters import BookmarkFilter
 
-# Create your views here.
+
+class BookmarkListView(generic.ListView):
+
+    model = Bookmark
+
+    def get_context_data(self, **kwargs):
+        context = super().get_context_data(**kwargs)
+        context["filter"] = BookmarkFilter(
+            self.request.GET, queryset=self.get_queryset()
+        )
+        return context
